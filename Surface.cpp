3cd07dc099c3d34d12163e6e3032e4f6f1c1cbf2
ChemrovKirill <chemrovks@phystech.edu>
2020-03-03 22:37:57 +0300

    Add texture to surface

--- Surface.cpp
+++ Surface.cpp
@@ -10,7 +10,7 @@ Surface::Surface() {
 	Vector2f point = center_position;
 	srand(time(NULL));
 	float slope_direction;
-	for (int i = 0; i < 2*vertex_count; ++i) {
+	for (int i = 0; i <= 2*vertex_count; ++i) {
 		/*switch (rand() % 4) 
 		{
 		case 0 :
@@ -28,7 +28,7 @@ Surface::Surface() {
 		}*/
 		surface.append(Vertex(point, Color::White));
 		surface[i].texCoords = Vector2f(x_spacing * ((i / 2) % 2), 700 - point.y);
-		surface.append(Vertex(Vector2f(point.x, 700), Color::White));
+		surface.append(Vertex(Vector2f(point.x, 700), Color::Red));
 		++i;
 		surface[i].texCoords = Vector2f(x_spacing * ((i / 2) % 2), 0);
 		slope_direction = ((float)(rand() % 100)) / 100 - 0.5f;
