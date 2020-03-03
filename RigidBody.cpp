0b81ac3002635453b01813831d99971cfc200f08
Ярослав Ивашкевич <yaroslav.ivashkevich@yandex.ru>
2020-03-03 23:13:31 +0300

    Add Control

--- RigidBody.cpp
+++ RigidBody.cpp
@@ -32,7 +32,7 @@ Force::Force() : is_force_field(false), force(0), force_vector(Vector2f(0, 0)),
 	force_point(Vector2f(0, 0)), exist(false) {};
 Force::Force(bool field, float new_force, Vector2f start_vector, Vector2f start_force_point)
 	: is_force_field(field), force(new_force), force_vector(start_vector), 
-	force_point(start_force_point), exist(true) {}
+	force_point(start_force_point), exist(false) {}
 
 
 
